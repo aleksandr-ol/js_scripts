b914af8f562f9e514427435504d88a5b3f702ef2
aleksandr-ol <oleksandr.oleniuk@gmail.com>
2020-11-28 16:55:51 +0200

    Create xhrInterceptor.js

---
+++ xhrInterceptor.js
@@ -0,0 +1,10 @@
+const origOpen = XMLHttpRequest.prototype.open;
+XMLHttpRequest.prototype.open = function() {
+    console.log('request started!');
+    this.addEventListener('load', function() {
+        console.log('request completed!');
+        console.log(this.responseURL); //will always be 4 (ajax is completed successfully)
+        console.log(this.responseText); //whatever the response was
+    });
+    origOpen.apply(this, arguments);
+};
